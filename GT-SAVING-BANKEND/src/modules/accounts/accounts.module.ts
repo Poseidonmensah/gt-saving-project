83321bb6a409ccc9826fe018154cd91f8283daca
Poseidonmensah <aidoor36@gmail.com>
2026-06-08 20:20:47 +0000

    Fixed architectural dupplicates in app, transactions, and accounts modules

--- GT-SAVING-BANKEND/src/modules/accounts/accounts.module.ts
+++ GT-SAVING-BANKEND/src/modules/accounts/accounts.module.ts
@@ -4,9 +4,11 @@ import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
 import { AccountEntity } from './entities/account.entity';
 import { AuditModule } from '../audit/audit.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { WorkflowModule } from '../workflow/workflow.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AccountEntity]), AuditModule],
+  imports: [TypeOrmModule.forFeature([AccountEntity]), AuditModule, NotificationsModule, WorkflowModule],
   controllers: [AccountsController],
   providers: [AccountsService],
   exports: [AccountsService, TypeOrmModule],
